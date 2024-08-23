99ce45e5fb6f9b7267bbb2d429a30c3184f2d419
RazBenLulu <Razbenlulu@gmail.com>
2024-08-23 16:42:59 +0300

    final - without server

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,7 +6,7 @@ import { Route, Routes, Outlet } from "react-router-dom";
 import Homepage from './components/homepage/Homepage';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-import Login from './components/login/Login';
+import Login from './components/loginPage/login';
 
 
 function App() {
--- front/src/components/homepage/Homepage.jsx
+++ front/src/components/homepage/Homepage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import Login from "../login/Login"
+import Login from "../loginPage/login"
 
 function Homepage(params) {
     return (

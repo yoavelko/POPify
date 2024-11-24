4d9382f685d1b16e4f03e7a1721367a473ce6501
Yoavelko <yoavelkobi889@gmail.com>
2024-11-24 17:37:47 +0200

    admin-orders

--- front/src/components/Admin/orders/Orders.jsx
+++ front/src/components/Admin/orders/Orders.jsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from 'react';
 import './Orders.css'
+import axios from 'axios';
+import { getAllOrders } from '../../../utils/OrderRoutes';
+import OrderBox from './orderBox/orderBox';
 
 function Orders() {
-    
+
+    const [allOrders, setAllOrders] = useState();
+    const [update, setUpdate] = useState(true);
+
+    const fetchOrders = async () => {
+        try {
+            const response = await axios.get(getAllOrders);
+            const data = response.data.orders;
+            console.log(data);
+            setAllOrders(data);
+        } catch (error) {
+            console.error("Error fetching products:", error.message);
+            setProducts([]);
+        }
+    }
+
+    useEffect(() => {
+        fetchOrders();
+    }, [update])
+
     return (
         <div className='admin-orders-container'>
-            orders
+            <div className='admin-orders-header'>Orders Managment</div>
+            {allOrders && allOrders.map((value, index) => (
+                <OrderBox key={index} value={value} setUpdate={setUpdate} update={update}/>
+            ))}
         </div>
     )
 }
--- front/src/utils/OrderRoutes.js
+++ front/src/utils/OrderRoutes.js
@@ -3,3 +3,4 @@ export const getUserOrders = `${orderHOST}/user-orders`;
 export const createOrder = `${orderHOST}/new-order`;
 export const updateOrderStatus = `${orderHOST}/update-status`;
 export const getProductsWithPopularity = `${orderHOST}/with-popularity`;
+export const getAllOrders = `${orderHOST}/get-all-orders`

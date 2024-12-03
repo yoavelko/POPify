913206ebb530ae2b5361cb1253c59419e7cffc29
Yoavelko <yoavelkobi889@gmail.com>
2024-12-03 18:46:06 +0200

    last

--- front/src/components/Admin/productA/newModal/newModal.jsx
+++ front/src/components/Admin/productA/newModal/newModal.jsx
@@ -29,7 +29,7 @@ function NewModal({ setIsModalOpen }) {
             alert('Category is required');
             return;
         }
-    
+
         try {
             const response = await axios.post('http://localhost:3001/admin/new-product', {
                 name: product.name,
@@ -37,7 +37,7 @@ function NewModal({ setIsModalOpen }) {
                 img: product.img, // מערך של שתי התמונות
                 category: product.category
             });
-    
+
             alert("New Product Added Successfully");
             setProducts((prevProducts) => [...prevProducts, response.data.product]);
             setIsModalOpen(false);
@@ -45,78 +45,80 @@ function NewModal({ setIsModalOpen }) {
             console.error("Error creating new product:", err.response?.data || err.message);
             alert("Failed to add the product. Please try again.");
         }
-    };    
+    };
 
     return (
-        <div className="modal">
-            <div className="modal-content">
-                <span className="close" onClick={() => setIsModalOpen(false)}>&times;</span>
-                <h3>Add New Product</h3>
-                <form onSubmit={(e) => { e.preventDefault(); handleSubmit(); }}>
-                    <label>
-                        Name:
-                        <input
-                            type="text"
-                            value={product.name}
-                            onChange={(e) => setProduct((prev) => ({ ...prev, name: e.target.value }))}
-                            required
-                        />
-                    </label>
-                    <label>
-                        Price:
-                        <input
-                            type="number"
-                            value={product.price}
-                            onChange={(e) => setProduct((prev) => ({ ...prev, price: e.target.value }))}
-                            required
-                        />
-                    </label>
-                    <label>
-                        First Image Link:
-                        <input
-                            type="text"
-                            value={product.img[0]}
-                            onChange={(e) =>
-                                setProduct((prev) => {
-                                    const updatedImg = [...prev.img];
-                                    updatedImg[0] = e.target.value;
-                                    return { ...prev, img: updatedImg };
-                                })
-                            }
-                            required
-                        />
-                    </label>
-                    <label>
-                        Second Image Link:
-                        <input
-                            type="text"
-                            value={product.img[1]}
-                            onChange={(e) =>
-                                setProduct((prev) => {
-                                    const updatedImg = [...prev.img];
-                                    updatedImg[1] = e.target.value;
-                                    return { ...prev, img: updatedImg };
-                                })
-                            }
-                            required
-                        />
-                    </label>
-                    <label>
-                        Category:
-                        <select
-                            value={product.category}
-                            onChange={(e) => setProduct((prev) => ({ ...prev, category: e.target.value }))}
-                            required
-                        >
-                            <option value="">Select Category</option>
-                            <option value="Marvel">Marvel</option>
-                            <option value="DC">DC</option>
-                            <option value="Disney">Disney</option>
-                            <option value="Harry Potter">Harry Potter</option>
-                        </select>
-                    </label>
-                    <button type="submit">Add Product</button>
-                </form>
+        <div className="new-modal-container">
+            <div className='new-modal-inner'>
+                <div className="modal-content">
+                    <span className="close" onClick={() => setIsModalOpen(false)}>&times;</span>
+                    <h3>Add New Product</h3>
+                    <form className='new-form' onSubmit={(e) => { e.preventDefault(); handleSubmit(); }}>
+                        <label>
+                            Name:
+                            <input
+                                type="text"
+                                value={product.name}
+                                onChange={(e) => setProduct((prev) => ({ ...prev, name: e.target.value }))}
+                                required
+                            />
+                        </label>
+                        <label>
+                            Price:
+                            <input
+                                type="number"
+                                value={product.price}
+                                onChange={(e) => setProduct((prev) => ({ ...prev, price: e.target.value }))}
+                                required
+                            />
+                        </label>
+                        <label>
+                            First Image Link:
+                            <input
+                                type="text"
+                                value={product.img[0]}
+                                onChange={(e) =>
+                                    setProduct((prev) => {
+                                        const updatedImg = [...prev.img];
+                                        updatedImg[0] = e.target.value;
+                                        return { ...prev, img: updatedImg };
+                                    })
+                                }
+                                required
+                            />
+                        </label>
+                        <label>
+                            Second Image Link:
+                            <input
+                                type="text"
+                                value={product.img[1]}
+                                onChange={(e) =>
+                                    setProduct((prev) => {
+                                        const updatedImg = [...prev.img];
+                                        updatedImg[1] = e.target.value;
+                                        return { ...prev, img: updatedImg };
+                                    })
+                                }
+                                required
+                            />
+                        </label>
+                        <label>
+                            Category:
+                            <select
+                                value={product.category}
+                                onChange={(e) => setProduct((prev) => ({ ...prev, category: e.target.value }))}
+                                required
+                            >
+                                <option value="">Select Category</option>
+                                <option value="Marvel">Marvel</option>
+                                <option value="DC">DC</option>
+                                <option value="Disney">Disney</option>
+                                <option value="Harry Potter">Harry Potter</option>
+                            </select>
+                        </label>
+                        <button type="submit">Add Product</button>
+                    </form>
+                </div>
             </div>
         </div>
     );
